eecdef80c19b07219212784e1ba1a661f617f95c
Dimmeh <dimmehgms@gmail.com>
2019-04-07 15:26:12 -0400

    rewrite python2 version to meet the goal

--- fib_py2.py
+++ fib_py2.py
@@ -1,18 +1,12 @@
-def fib(num):
-	"""
-	Fn = Fn-1 + Fn-2
-	"""
-	if num == 0:
-		return 0
-	elif num == 1:
-		return 1
-	else:
-		return (fib(num-1) + fib(num-2))
-
 def main():
-	num = raw_input("enter a positive integer: ")
-	res = fib(int(num))
-	print(res)
+    n = 90
+    p = 0
+    q = 1
+    for i in range(n):
+        fib = p + q
+        p = q
+        q = fib
+        print("{:20}".format(p))
 
 if __name__ == '__main__':
-	main()
+    main()
